192db5d11db8b09f3b882119970f3ef334fa6ea0
Melwin Kramer <melwin.kramer@gmail.com>
2023-11-20 21:48:26 +0100

    Add dark theme CSS and update avatar

--- src/scripts/contentScript.js
+++ src/scripts/contentScript.js
@@ -1,6 +1,26 @@
 // This script is injected into the page when the extension is activated.
 // Made by Melwin Kramer 2023
 
+const injectCustomCss = () => {
+
+    // Work in progress custom dark theme
+    /*const customCss = document.createElement("link");
+    customCss.rel = "stylesheet";
+    customCss.href = chrome.runtime.getURL("src/styles/darkTheme.css"); // THIS IS SPECIAL CHROME CODE, INCOMPATIBLE WITH FIREFOX AND OTHER BROWSERS
+    document.head.appendChild(customCss);*/
+
+    // Replace basic avatar with cat avatar
+    const avatarObserver = setInterval(() => {
+        const avatars = document.querySelectorAll("img");
+        // check if avatar is include in a class name
+        avatars.forEach(avatar => {
+            if (avatar.src.includes("nobody")) {
+                avatar.src = "https://natusan.co.uk/cdn/shop/articles/natusan-blog-how-cat-years-work-header_600x600_crop_center.jpg?v=1674474680"
+            }
+        });
+    }, 500); // 500ms interval, possiblily to low
+}
+
 const createFileObserver = () => {
   const fileObserver = setInterval(() => {
     const fileSource = document.querySelector("iframe")?.src || undefined;
@@ -24,8 +44,10 @@ const createFileObserver = () => {
 
 const init = async () => {
     /* This function is async everything initialized here should start without await(ing) something! */
-
     createFileObserver();
+    injectCustomCss();
 };
 
-init();
+window.addEventListener("load", () => {
+    init(); // Start the init function when the page is loaded
+});
